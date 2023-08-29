dd76f2de4405710d1663404a8988596c4e1f8bce
juanathebanana <91966932+juanathebanana@users.noreply.github.com>
2023-08-29 18:41:17 +0200

    pairplots.py

---
+++ pairplots.py
@@ -0,0 +1,4 @@
+#Also called "scatterplot matrices", these kinds of plots are useful to explore correlations in multidimensional data.
+sns.pairplot(iris, hue="species", height=3); # height = h. (inches) of each facet
+
+#https://seaborn.pydata.org/generated/seaborn.pairplot.html
