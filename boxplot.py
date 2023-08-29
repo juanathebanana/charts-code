3a394cf91679c5d73c813968f21e182a51919d31
juanathebanana <91966932+juanathebanana@users.noreply.github.com>
2023-08-29 18:17:20 +0200

    boxplot.py

---
+++ boxplot.py
@@ -0,0 +1,10 @@
+df.loc[['Italy', 'Germany'], years].transpose()
+df_ig.plot(kind='box')
+
+plt.title("title")
+plt.ylabel("ylbael")
+
+plt.show()
+
+
+#https://www.geeksforgeeks.org/box-plot-in-python-using-matplotlib/
