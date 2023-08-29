651ea73147143f0168332960fe4514cc887ac746
juanathebanana <91966932+juanathebanana@users.noreply.github.com>
2023-08-29 18:58:18 +0200

    scatterplot.py

--- scatterplot.py
+++ scatterplot.py
@@ -3,3 +3,11 @@ df.plot(kind='scatter', x='year', y='total')
 
 #https://matplotlib.org/stable/gallery/shapes_and_collections/scatter.html
 #https://www.geeksforgeeks.org/matplotlib-pyplot-scatter-in-python/
+
+#sns
+sns.scatterplot(tips, x="total_bill", y="tip")
+#Seaborn supports statistical estimation, for example of linear regression lines:
+sns.lmplot(tips, x="total_bill", y="tip");
+#Stratification by a third variable is supported by the argument `hue`:
+sns.scatterplot(tips, x="total_bill", y="tip", hue="time");
+sns.lmplot(tips, x="total_bill", y="tip", hue="time", col="smoker");
