26dc6ffbb486230ee7c92c3117840c6d43b9b775
juanathebanana <91966932+juanathebanana@users.noreply.github.com>
2023-08-29 18:20:43 +0200

    scatterplot.py

---
+++ scatterplot.py
@@ -0,0 +1,5 @@
+df.plot(kind='scatter', x='year', y='total')
+
+
+#https://matplotlib.org/stable/gallery/shapes_and_collections/scatter.html
+#https://www.geeksforgeeks.org/matplotlib-pyplot-scatter-in-python/
